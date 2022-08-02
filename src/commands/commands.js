4981d036d21a494a3280c3d88a2856e46e03404f
n.grishkin <n.grishkin@iva.ru>
2022-08-02 18:26:09 +0500

    initial commit

---
+++ src/commands/commands.js
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global global, Office, self, window */
+
+let isReady = false;
+let interval$;
+
+Office.onReady(() => {
+  // If needed, Office.js is ready to be called
+  isReady = true;
+});
+
+function displayDialogAsync(dialogUrl, dialogOptions) {
+  return new Promise((resolve) => {
+    const dialogClosed = async () => {
+      resolve();
+    };
+
+    Office.context.ui.displayDialogAsync(dialogUrl, dialogOptions, (result) => {
+      let dialog = result.value;
+      dialog.addEventHandler(Office.EventType.DialogEventReceived, dialogClosed);
+    });
+  });
+}
+
+/**
+ * Shows a notification when the add-in command is executed.
+ * @param event {Office.AddinCommands.Event}
+ */
+function action(event) {
+  // const message = {
+  //   type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+  //   message: "Performed action.",
+  //   icon: "Icon.80x80",
+  //   persistent: true,
+  // };
+
+  // Show a notification message
+  // Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
+
+  displayDialogAsync("/dialog.html", {
+    width: 30,
+    height: 20,
+  }).then(() => {
+    event.completed();
+  });
+
+  // Be sure to indicate when the add-in command function is complete
+  // setTimeout(() => event.completed(), 30000);
+}
+
+function itemSentHolder(event) {
+  displayDialogAsync("/dialog.html", {
+    width: 30,
+    height: 20,
+  }).then(() => {
+    event.completed({ allowEvent: true });
+  });
+}
+
+function itemSent(event) {
+  interval$ = setInterval(() => {
+    if (isReady) {
+      clearInterval(interval$);
+      itemSentHolder(event);
+    }
+  }, 200);
+}
+
+function getGlobal() {
+  return typeof self !== "undefined"
+    ? self
+    : typeof window !== "undefined"
+    ? window
+    : typeof global !== "undefined"
+    ? global
+    : undefined;
+}
+
+const g = getGlobal();
+
+// The add-in command functions need to be available in global scope
+g.action = action;
+g.itemSent = itemSent;
---
+++ src/launchevent/launchevent.js
@@ -0,0 +1,67 @@
+function displayDialogAsync(dialogUrl, dialogOptions) {
+  return new Promise((resolve) => {
+    const dialogClosed = async () => {
+      resolve();
+    };
+
+    Office.context.ui.displayDialogAsync(dialogUrl, dialogOptions, (result) => {
+      let dialog = result.value;
+      dialog.addEventHandler(Office.EventType.DialogEventReceived, dialogClosed);
+    });
+  });
+}
+
+function holder(event, message) {
+  displayDialogAsync("/dialog.html", {
+    width: 30,
+    height: 20,
+  });
+}
+
+function onMessageComposeHandler(event) {
+  holder(event, "onMessageComposeHandler");
+}
+
+function onAppointmentComposeHandler(event) {
+  holder(event, "onAppointmentComposeHandler");
+}
+
+function onMessageAttachmentsChangedHandler(event) {
+  holder(event, "onMessageAttachmentsChangedHandler");
+}
+
+function onAppointmentAttachmentsChangedHandler(event) {
+  holder(event, "onAppointmentAttachmentsChangedHandler");
+}
+
+function onMessageRecipientsChangedHandler(event) {
+  holder(event, "onMessageRecipientsChangedHandler");
+}
+
+function onAppointmentAttendeesChangedHandler(event) {
+  holder(event, "onAppointmentAttendeesChangedHandler");
+}
+
+function onAppointmentTimeChangedHandler(event) {
+  holder(event, "onAppointmentTimeChangedHandler");
+}
+
+function onAppointmentRecurrenceChangedHandler(event) {
+  holder(event, "onAppointmentRecurrenceChangedHandler");
+}
+
+function onInfobarDismissClickedHandler(event) {
+  holder(event, "onInfobarDismissClickedHandler");
+}
+
+// 1st parameter: FunctionName of LaunchEvent in the manifest; 2nd parameter: Its implementation in this .js file.
+Office.actions.associate("onMessageComposeHandler", onMessageComposeHandler);
+Office.actions.associate("onAppointmentComposeHandler", onAppointmentComposeHandler);
+
+Office.actions.associate("onMessageAttachmentsChangedHandler", onMessageAttachmentsChangedHandler);
+Office.actions.associate("onAppointmentAttachmentsChangedHandler", onAppointmentAttachmentsChangedHandler);
+Office.actions.associate("onMessageRecipientsChangedHandler", onMessageRecipientsChangedHandler);
+Office.actions.associate("onAppointmentAttendeesChangedHandler", onAppointmentAttendeesChangedHandler);
+Office.actions.associate("onAppointmentTimeChangedHandler", onAppointmentTimeChangedHandler);
+Office.actions.associate("onAppointmentRecurrenceChangedHandler", onAppointmentRecurrenceChangedHandler);
+Office.actions.associate("onInfobarDismissClickedHandler", onInfobarDismissClickedHandler);
